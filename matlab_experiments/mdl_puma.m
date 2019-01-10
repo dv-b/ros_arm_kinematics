e64037d4301b6404bafd6e54c28009bb61da9c60
Bulat <davlyatshinbulat@gmail.com>
2019-01-10 23:56:36 +0100

    matlab robot plotting

--- matlab_experiments/mdl_puma.m
+++ matlab_experiments/mdl_puma.m
@@ -1,3 +1,16 @@
 mdl_puma560
 p560
+
+p560.base = transl(10, 15, 2) * trotx(pi)
+
+p560.tool = transl(0, 0, 0.2)
+
 p560.fkine([0.1 0.2 0.3 0 0 0])
+
+T = transl(0.6, 0.1, 0) * rpy2tr(0, 180, 0, 'deg');
+
+ql = p560.ikine6s(T, 'l');
+
+qr = p560.ikine6s(T, 'r');
+
+p560.plot(ql);
