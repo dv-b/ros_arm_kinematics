9ec360a2f1df1fedd317166acf3baa33bfa53d8c
Bulat <davlyatshinbulat@gmail.com>
2019-01-11 23:28:10 +0100

    matlab robot plotting and moving

--- matlab_experiments/mdl_puma.m
+++ matlab_experiments/mdl_puma.m
@@ -2,11 +2,11 @@ mdl_puma560
 
 p560
 
-p560.base = transl(10, 15, 2) * trotx(pi)
-
-p560.tool = transl(0, 0, 0.2)
-
-p560.fkine([0.1 0.2 0.3 0 0 0])
+% % p560.base = transl(10, 15, 2) * trotx(pi)
+% 
+% p560.tool = transl(0, 0, 0.2)
+% 
+% p560.fkine([0.1 0.2 0.3 0 0 0])
 
 T = transl(0.6, 0.1, 0) * rpy2tr(0, 180, 0, 'deg');
 
@@ -14,4 +14,7 @@ ql = p560.ikine6s(T, 'l');
 
 qr = p560.ikine6s(T, 'r');
 
-p560.plot(qr);
+p560.plot(qz);
+
+tg = jtraj(qz, ql, 50);
+
